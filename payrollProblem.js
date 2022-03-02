c70e262ac09cf0ab89354083b2bd4d5d51f89d01
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2022-03-02 12:34:36 +1100

    accepts CLI commands for CSV input and output files

--- payrollProblem.js
+++ payrollProblem.js
@@ -5,16 +5,16 @@ const ObjectsToCsv = require('objects-to-csv')
 const createPayslip = require('./createPaySlip');
 const printPaySlip = require('./printPaySlip');
 
-const csvInput = fs.readFileSync('sample_input.csv').toString()
+const csvInput = fs.readFileSync(process.argv[2]).toString()
 
 parse(csvInput, {}, (err, records) => {
         const individualPayslipInfos = records.slice(1).map(record => createPayslip(interpretCSVInput(record)));
-        saveToCSV(individualPayslipInfos, 'example_output')
+        saveToCSV(individualPayslipInfos, process.argv[3])
     });
     
-    const saveToCSV = async (payslips, filename) => {
+    const saveToCSV = async (payslips, outputFilename) => {
         const csv = new ObjectsToCsv(payslips);
-        await csv.toDisk(`./${filename}.csv`)
+        await csv.toDisk(`./${outputFilename}`)
         return 1
 } 
 
@@ -33,44 +33,44 @@ const interpretCSVInput = record => {
         return personalDetails
 }
 
-prompt.message = 'Please enter your ';
-prompt.delimiter = '';
-prompt.colors = false;
+// prompt.message = 'Please enter your ';
+// prompt.delimiter = '';
+// prompt.colors = false;
 
-const schema = {
-    properties: {
-        name: {
-            pattern: /^[a-zA-Z\s\-]+$/,
-            message: 'Name may only contain letters, spaces, or dashes',
-            required: true
-        },
-        surname: {
-            pattern: /^[a-zA-Z\s\-]+$/,
-            message: 'Surname may only contain letters, spaces, or dashes',
-            required: true
-        },
-        'annual salary': {
-            pattern: /^[0-9]+$/,
-            message: 'Salary must be a positive integer',
-            required: true
-        },
-        'super rate': {
-            pattern: /^([0-9]|[1-4][0-9]|50)$/,
-            message: 'Salary must be a positive integer between 0 and 50',
-            required: true
-        },
-        'start date': {
-            pattern: /^0?1 (January|February|March|April|May|June|July|August|September|October|November|December)$/,
-            message: 'Date must be the first day of a month e.g. 1 February',
-            required: true
-        },
-        'end date': {
-            pattern: /^((31 (January|March|May|July|August|October|December))|(2[89] February)|(30 (September|April|June|November)))$/,
-            message: 'Date must be the last day of a month e.g. 30 September',
-            required: true
-        },
-    }
-}
+// const schema = {
+//     properties: {
+//         name: {
+//             pattern: /^[a-zA-Z\s\-]+$/,
+//             message: 'Name may only contain letters, spaces, or dashes',
+//             required: true
+//         },
+//         surname: {
+//             pattern: /^[a-zA-Z\s\-]+$/,
+//             message: 'Surname may only contain letters, spaces, or dashes',
+//             required: true
+//         },
+//         'annual salary': {
+//             pattern: /^[0-9]+$/,
+//             message: 'Salary must be a positive integer',
+//             required: true
+//         },
+//         'super rate': {
+//             pattern: /^([0-9]|[1-4][0-9]|50)$/,
+//             message: 'Salary must be a positive integer between 0 and 50',
+//             required: true
+//         },
+//         'start date': {
+//             pattern: /^0?1 (January|February|March|April|May|June|July|August|September|October|November|December)$/,
+//             message: 'Date must be the first day of a month e.g. 1 February',
+//             required: true
+//         },
+//         'end date': {
+//             pattern: /^((31 (January|March|May|July|August|October|December))|(2[89] February)|(30 (September|April|June|November)))$/,
+//             message: 'Date must be the last day of a month e.g. 30 September',
+//             required: true
+//         },
+//     }
+// }
 
 // prompt.get(schema, (err, result) => {
 //         const payslipInfo = createPayslip(result)
